ad67c4c0a37d9e8b973c4d6460730adf7a4e0c66
Kairos-T <contact@kairostay.com>
2024-08-10 18:55:45 +0800

    chore: fix typo in SMTP_RECIPIENT

--- tests/email_traffic.py
+++ tests/email_traffic.py
@@ -3,4 +3,4 @@ from utils.network_utils import test_smtp_connection
 
 def run_tests():
     print("Running Email Traffic Tests")
-    test_smtp_connection(config.SMTP_SERVER, config.SMTP_PORT, config.SMTP_USERNAME, config.SMTP_PASSWORD, config.SMTP_RECIPEINT)
+    test_smtp_connection(config.SMTP_SERVER, config.SMTP_PORT, config.SMTP_USERNAME, config.SMTP_PASSWORD, config.SMTP_RECIPIENT)
